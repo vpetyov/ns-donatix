0b1ea0a5b93b7f4066a52249cc40df8c6e7145a6
Ivan Ugrin <ivan.ugrin@gmail.com>
2019-05-09 14:26:01 +0300

    Add native functionality

--- src/ns-donatix.android.ts
+++ src/ns-donatix.android.ts
@@ -1,5 +1,19 @@
 import { Common } from './ns-donatix.common';
+import application from "tns-core-modules/application";
 
 export class NsDonatix extends Common {
+  public greet() {
+    "use strict";
+    var Toast = application.android.widget.Toast;
+    var text = "Hello world!";
 
+    var centeredText = new application.android.text.SpannableString(text);
+    centeredText.setSpan(
+        new application.android.text.style.AlignmentSpan.Standard(application.android.text.Layout.Alignment.ALIGN_CENTER),
+        0,
+        text.length - 1,
+        application.android.text.Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+    return Toast.makeText(application.android.context, centeredText, Toast.LENGTH_LONG);
+  }
 }
--- src/ns-donatix.ios.ts
+++ src/ns-donatix.ios.ts
@@ -1,5 +1,37 @@
 import { Common } from './ns-donatix.common';
+var application = require("application");
 
 export class NsDonatix extends Common {
+    public greet() {
+        let alertController = UIAlertController.alertControllerWithTitleMessagePreferredStyle("", "Hello world!", UIAlertControllerStyle.Alert);
 
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .destructive, handler: nil));
+
+        showUIAlertController(alertController);
+    }
+
+    function showUIAlertController(alertController: UIAlertController) {
+        let currentView = application.getRootView();
+    
+        if (currentView) {
+            currentView = currentView.modal || currentView;
+    
+            let viewController: UIViewController = currentView.ios;
+    
+            if (!(currentView.ios instanceof UIViewController)) {
+                const parentWithController = application.ios.getParentWithViewController(currentView);
+                viewController = parentWithController ? parentWithController.viewController : undefined;
+            }
+    
+            if (viewController) {
+                if (alertController.popoverPresentationController) {
+                    alertController.popoverPresentationController.sourceView = viewController.view;
+                    alertController.popoverPresentationController.sourceRect = CGRectMake(viewController.view.bounds.size.width / 2.0, viewController.view.bounds.size.height / 2.0, 1.0, 1.0);
+                    alertController.popoverPresentationController.permittedArrowDirections = 0;
+                }
+    
+                viewController.presentModalViewControllerAnimated(alertController, true);
+            }
+        }
+    }
 }
